398d8cfa2aa205834cd4071e7e54ae13a3facd5c
Dave Raynor <RaynorD@users.noreply.github.com>
2017-11-15 16:11:07 -0600

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace DeploySymlinks
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnBrowseSrc = new System.Windows.Forms.Button();
+			this.txtSrc = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.txtDeploy = new System.Windows.Forms.TextBox();
+			this.btnBrowseDeploy = new System.Windows.Forms.Button();
+			this.btnGo = new System.Windows.Forms.Button();
+			this.txtOutput = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.txtWildcard = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// btnBrowseSrc
+			// 
+			this.btnBrowseSrc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnBrowseSrc.Location = new System.Drawing.Point(729, 25);
+			this.btnBrowseSrc.Name = "btnBrowseSrc";
+			this.btnBrowseSrc.Size = new System.Drawing.Size(75, 23);
+			this.btnBrowseSrc.TabIndex = 0;
+			this.btnBrowseSrc.Text = "Browse...";
+			this.btnBrowseSrc.UseVisualStyleBackColor = true;
+			this.btnBrowseSrc.Click += new System.EventHandler(this.BtnBrowseSrc_Click);
+			// 
+			// txtSrc
+			// 
+			this.txtSrc.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtSrc.Location = new System.Drawing.Point(12, 27);
+			this.txtSrc.Name = "txtSrc";
+			this.txtSrc.Size = new System.Drawing.Size(711, 20);
+			this.txtSrc.TabIndex = 1;
+			this.txtSrc.Text = "C:\\Users\\dkbso\\Documents\\Arma 3 - Other Profiles\\CW2%2eRaynor%2eD\\missions\\!Rayno" +
+    "rsTools\\master\\RaynorA3Tools";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(89, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Source Directory:";
+			// 
+			// label2
+			// 
+			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 269);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(122, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Deploy Parent Directory:";
+			// 
+			// txtDeploy
+			// 
+			this.txtDeploy.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtDeploy.Location = new System.Drawing.Point(11, 287);
+			this.txtDeploy.Name = "txtDeploy";
+			this.txtDeploy.Size = new System.Drawing.Size(711, 20);
+			this.txtDeploy.TabIndex = 4;
+			this.txtDeploy.Text = "C:\\Users\\dkbso\\Documents\\Arma 3 - Other Profiles\\CW2%2eRaynor%2eD\\missions";
+			// 
+			// btnBrowseDeploy
+			// 
+			this.btnBrowseDeploy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnBrowseDeploy.Location = new System.Drawing.Point(728, 285);
+			this.btnBrowseDeploy.Name = "btnBrowseDeploy";
+			this.btnBrowseDeploy.Size = new System.Drawing.Size(75, 23);
+			this.btnBrowseDeploy.TabIndex = 3;
+			this.btnBrowseDeploy.Text = "Browse...";
+			this.btnBrowseDeploy.UseVisualStyleBackColor = true;
+			this.btnBrowseDeploy.Click += new System.EventHandler(this.BtnBrowseDeploy_Click);
+			// 
+			// btnGo
+			// 
+			this.btnGo.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.btnGo.Location = new System.Drawing.Point(363, 361);
+			this.btnGo.Name = "btnGo";
+			this.btnGo.Size = new System.Drawing.Size(92, 29);
+			this.btnGo.TabIndex = 6;
+			this.btnGo.Text = "Go";
+			this.btnGo.UseVisualStyleBackColor = true;
+			this.btnGo.Click += new System.EventHandler(this.BtnGo_Click);
+			// 
+			// txtOutput
+			// 
+			this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtOutput.Location = new System.Drawing.Point(12, 54);
+			this.txtOutput.Multiline = true;
+			this.txtOutput.Name = "txtOutput";
+			this.txtOutput.ReadOnly = true;
+			this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.txtOutput.Size = new System.Drawing.Size(792, 204);
+			this.txtOutput.TabIndex = 7;
+			this.txtOutput.WordWrap = false;
+			// 
+			// label3
+			// 
+			this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(13, 317);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(84, 13);
+			this.label3.TabIndex = 9;
+			this.label3.Text = "Folder Wildcard:";
+			// 
+			// txtWildcard
+			// 
+			this.txtWildcard.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtWildcard.Location = new System.Drawing.Point(12, 335);
+			this.txtWildcard.Name = "txtWildcard";
+			this.txtWildcard.Size = new System.Drawing.Size(791, 20);
+			this.txtWildcard.TabIndex = 8;
+			this.txtWildcard.Text = "Alpha_1-7_Training";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(816, 399);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.txtWildcard);
+			this.Controls.Add(this.txtOutput);
+			this.Controls.Add(this.btnGo);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.txtDeploy);
+			this.Controls.Add(this.btnBrowseDeploy);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtSrc);
+			this.Controls.Add(this.btnBrowseSrc);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnBrowseSrc;
+		private System.Windows.Forms.TextBox txtSrc;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox txtDeploy;
+		private System.Windows.Forms.Button btnBrowseDeploy;
+		private System.Windows.Forms.Button btnGo;
+		private System.Windows.Forms.TextBox txtOutput;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox txtWildcard;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace DeploySymlinks
+{
+
+	public partial class Form1 : Form
+	{
+		[DllImport("kernel32.dll")]
+		static extern bool CreateSymbolicLink(string lpSymlinkFileName, string lpTargetFileName, SymbolicLink dwFlags);
+
+		enum SymbolicLink
+		{
+			File = 0,
+			Directory = 1
+		}
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Log(string text)
+		{
+			txtOutput.Text += Environment.NewLine + text;
+		}
+
+		private void Log(string[] text)
+		{
+			txtOutput.Text += Environment.NewLine + string.Join(Environment.NewLine, text);
+		}
+
+		private void UserSelectFolder(TextBox box)
+		{
+			FolderBrowserDialog dialog = new FolderBrowserDialog();
+			if(!string.IsNullOrEmpty(box.Text) && Directory.Exists(box.Text))
+			{
+				dialog.SelectedPath = box.Text;
+			}
+			DialogResult res = dialog.ShowDialog();
+
+			if (res == DialogResult.OK && !string.IsNullOrWhiteSpace(dialog.SelectedPath))
+			{
+				box.Text = dialog.SelectedPath;
+			}
+		}
+
+		private void BtnBrowseSrc_Click(object sender, EventArgs e)
+		{
+			UserSelectFolder(txtSrc);
+		}
+
+		private void BtnBrowseDeploy_Click(object sender, EventArgs e)
+		{
+			UserSelectFolder(txtDeploy);
+		}
+
+		private void BtnGo_Click(object sender, EventArgs e)
+		{
+			if(string.IsNullOrWhiteSpace(txtSrc.Text) || !Directory.Exists(txtSrc.Text))
+			{
+				Log("Source folder not valid");
+				return;
+			}
+
+			if (string.IsNullOrWhiteSpace(txtDeploy.Text) || !Directory.Exists(txtDeploy.Text))
+			{
+				Log("Deploy folder not valid");
+				return;
+			}
+
+			if (string.IsNullOrWhiteSpace(txtWildcard.Text))
+			{
+				Log("Wildcard text not valid");
+				return;
+			}
+
+			Log("=============================");
+
+			DirectoryInfo dirSrc = new DirectoryInfo(txtSrc.Text);
+			DirectoryInfo dirDeploy = new DirectoryInfo(txtDeploy.Text);
+			string wildcard = txtWildcard.Text;
+
+			Log("Source Directory: " + dirSrc.FullName);
+			Log("Deploy Parent Directory: " + dirDeploy.FullName);
+			Log("Folder Wildcard: " + wildcard);
+
+			Log("--------------------------");
+
+			Log("Source directories:");
+
+			string[] dirs = Directory.GetDirectories(txtSrc.Text);
+			var filtered = dirs.Where(d => !(new DirectoryInfo(d)).Attributes.HasFlag(FileAttributes.Hidden));
+			Log(filtered.ToArray());
+
+			Log("--------------------------");
+		}
+	}
+}
